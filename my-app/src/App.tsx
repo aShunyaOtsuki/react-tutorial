e6b8227c7362d8d06ad22fd0dd54ebf5efdcf0c1
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-07-26 17:58:01 +0900

    add: reducer

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./reducer";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>arrow function style</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>arrow function style</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Provider>
   );
 };
 
