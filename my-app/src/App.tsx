ebac1d2e5fa1a20b70d4954432c7c56fefcec864
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-07-27 18:52:52 +0900

    add: Routes and Route

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,24 +3,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./reducer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Profile } from "./Profile";
+import { Home } from "./Home";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>arrow function style</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>arrow function style</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        <Routes>
+          <Route path="profile" element={<Profile />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };
