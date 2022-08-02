6c4e8e142bd4191f33cab6ad8e7f45526341602e
aShunyaOtuski <shunya.otsuki@access-company.com>
2022-08-02 17:30:02 +0900

    add: start-code

--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -1,3 +1,51 @@
+const Square = () => {
+  return <button className="square"></button>;
+};
+
+const Board = () => {
+  const status = "Next player: X";
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div></div>
+        <ol></ol>
+      </div>
+    </div>
+  );
+};
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Game />
+    </div>
+  );
 };
